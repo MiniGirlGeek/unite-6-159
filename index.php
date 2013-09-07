4c7d589175a45b0c302918e00a3e8a2311d3d95b
MiniGirlGeek <minigirlgeek@gmail.com>
2013-09-07 17:55:06 +0100

    Added date and article block for posts

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 while (have_posts()):
 	the_post(); ?>
-	<h1><?php the_title(); ?></h1>
-	<?php the_content();
-endwhile;
+	<article>
+		<div class="date"><?php echo get_the_date(); echo " at " . get_the_time(); ?></div>
+		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<?php the_content(); ?>
+	</article>
+<?php endwhile;
 
 get_footer();
