c4bbc2e0f96341d025537fe76a8d06ebc63c8d3e
MiniGirlGeek <minigirlgeek@gmail.com>
2013-09-07 15:56:29 +0100

    added wp head, footer, title

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Home</title>
+		<title><?php wp_title('| ', true, 'right'); bloginfo('title'); ?></title>
 		<link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet" type="text/css" />
+		<?php wp_head(); ?>
 	</head>
-	<body>	
+	<body>
+	
